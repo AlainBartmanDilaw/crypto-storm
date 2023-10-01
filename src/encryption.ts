15217c0c9c459b89453b6adb8dd3d0f056fbabde
Alain Léglise <fraternite76-alain@yahoo.fr>
2023-10-01 14:08:20 +0200

    Add type to objects and methods

--- src/encryption.ts
+++ src/encryption.ts
@@ -2,13 +2,13 @@
 
 import * as crypto from 'crypto';
 
-const IV_LENGTH = 16; // For AES, this is always 16
-const SETFIRST_MESSAGE = "Set first environment variable ENCRYPTION_KEY."
-const ERROR_MESSAGE = "The encryption key must be defined with 32 characters length string.";
-const UNDEFINED_MESSAGE = "The variable ENCRYPTION_KEY must be defined before any decrypt action.";
-const NO_UNDEFINED_TEXT = "The text you want to crypt or decrypt cannot be undefined or empty.";
+const IV_LENGTH: number = 16; // For AES, this is always 16
+const SETFIRST_MESSAGE: string = "Set first environment variable ENCRYPTION_KEY."
+const ERROR_MESSAGE: string = "The encryption key must be defined with 32 characters length string.";
+const UNDEFINED_MESSAGE: string = "The variable ENCRYPTION_KEY must be defined before any decrypt action.";
+const NO_UNDEFINED_TEXT: string = "The text you want to crypt or decrypt cannot be undefined or empty.";
 
-function getEncryptionKey() {
+function getEncryptionKey(): string {
     if (process.env.ENCRYPTION_KEY === undefined) {
         throw UNDEFINED_MESSAGE + " " + SETFIRST_MESSAGE;
     }
@@ -21,12 +21,12 @@ function getEncryptionKey() {
 // Our encryption key.
 // It's often generated randomly but it must be the same for decryption
 // Set first environment variable ENCRYPTION_KEY
-const ENCRYPTION_KEY = getEncryptionKey();
+const ENCRYPTION_KEY: string = getEncryptionKey();
 
 //Encryption algorithm
-const algorithm = 'aes-256-cbc';
+const algorithm: string = 'aes-256-cbc';
 
-function doEncrypt(text) {
+function doEncrypt(text: string): string {
 
     if (ENCRYPTION_KEY === undefined) {
         throw ERROR_MESSAGE;
@@ -36,16 +36,16 @@ function doEncrypt(text) {
         throw NO_UNDEFINED_TEXT;
     }
 
-    let iv = crypto.randomBytes(IV_LENGTH).slice(0, 16);
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(ENCRYPTION_KEY), iv);
-    let encrypted = cipher.update(text);
+    let iv: Buffer = crypto.randomBytes(IV_LENGTH).slice(0, 16);
+    let cipher: crypto.Cipher = crypto.createCipheriv(algorithm, Buffer.from(ENCRYPTION_KEY), iv);
+    let encrypted: Buffer = cipher.update(text);
 
     encrypted = Buffer.concat([ encrypted, cipher.final() ]);
 
     return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
-function doDecrypt(text) {
+function doDecrypt(text: string): string {
     if (ENCRYPTION_KEY === undefined) {
         throw ERROR_MESSAGE;
     }
@@ -54,11 +54,11 @@ function doDecrypt(text) {
         throw NO_UNDEFINED_TEXT;
     }
 
-    let textParts = text.split(':');
-    let iv = Buffer.from(textParts.shift(), 'hex');
-    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(ENCRYPTION_KEY), iv);
-    let decrypted = decipher.update(encryptedText);
+    let textParts: string[] = text.split(':');
+    let iv: Buffer = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText: Buffer = Buffer.from(textParts.join(':'), 'hex');
+    let decipher: crypto.Decipher = crypto.createDecipheriv(algorithm, Buffer.from(ENCRYPTION_KEY), iv);
+    let decrypted: Buffer = decipher.update(encryptedText);
 
     decrypted = Buffer.concat([ decrypted, decipher.final() ]);
 
